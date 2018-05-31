5da317698be385471fa558de4d78d8e2514bd679
Илья <Grinch@localhost.localdomain>
2018-05-31 21:17:22 +0700

    Fix bags in function 'digit_check'

--- src/game.h
+++ src/game.h
@@ -7,13 +7,15 @@ void string_to_int(int *digitmas);
 
 void print_result(int bulls, int *arr, int attempts);
 
+int getrand(int min, int max);
+
 void randomizer(int *mas);
 
 int bulls_counter(int *array_b, int *player_num);
 
 int cows_counter(int *array_c, int *player_n);
 
-int is_digit_check(const char *str);
+int digit_check(const char *str);
 
 int startup_options_check(int argc, char **argv);
 
