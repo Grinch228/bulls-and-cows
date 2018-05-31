7a4784370d23ddfbcc054a78922bb34b75b2d693
Илья <Grinch@localhost.localdomain>
2018-05-31 22:29:19 +0700

    Add tests for function 'bulls_counter'

--- test/tests.c
+++ test/tests.c
@@ -10,6 +10,19 @@ CTEST(checkers, repeat_digit_incorrect_input) {
 }
 
 CTEST(checkers, repeat_digit_incorrect_input) {
+    int result = repeat_digit("56788");
+    int expect = 0;
+    ASSERT_EQUAL(expect,result);
+}
+
+CTEST(checkers, repeat_digit_incorrect_input) {
+    int result = repeat_digit("5678 8");
+    int expect = 0;
+    ASSERT_EQUAL(expect,result);
+}
+
+
+CTEST(checkers, repeat_digit_incorrect_input) {
     int result = repeat_digit("5567");
     int expect = 0;
     ASSERT_EQUAL(expect,result);
@@ -79,4 +92,52 @@ CTEST(checkers, digit_check_correct_input) {
     int result = digit_check("5678");
     int expect = 1;
     ASSERT_EQUAL(expect,result);
+}
+
+CTEST(checkers, digit_check_incorrect_input) {
+    int result = digit_check("5678 5678");
+    int expect = 0;
+    ASSERT_EQUAL(expect,result);
+}
+
+CTEST(checkers, digit_check_correct_input) {
+    int result = digit_check("5678 rrrrr");
+    int expect = 0;
+    ASSERT_EQUAL(expect,result);
+}
+
+CTEST(checkers, digit_check_correct_input) {
+    int result = digit_check("5678 4321 rr");
+    int expect = 0;
+    ASSERT_EQUAL(expect,result);
+}
+
+CTEST(game, bulls_counter_guess_0) {
+    int result = bulls_counter(1234, 5678);
+    int expect = 0;
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(game, bulls_counter_guess_1) {
+    int result = bulls_counter(1234, 5674);
+    int expect = 1;
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(game, bulls_counter_guess_2) {
+    int result = bulls_counter(1234, 5634);
+    int expect = 2;
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(game, bulls_counter_guess_3) {
+    int result = bulls_counter(1234, 5234);
+    int expect = 3;
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(game, bulls_counter_guess_4) {
+    int result = bulls_counter(1234, 1234);
+    int expect = 4;
+    ASSERT_EQUAL(expect, result);
 }
